2a083ea4b63e3432c31acf5cee7e6caf99ae11f1
IbrahimFadel <ibrahim.fadel@ucc.on.ca>
2019-04-19 21:58:09 -0400

    CLEAN haha

--- src/js/quickAnims.js
+++ src/js/quickAnims.js
@@ -1,4 +1,87 @@
 /*
+ * Get all target in the config and return them in a usable format
+ *
+ * @param {OBJECT} config
+ * @return {ARRAY} targets
+ */
+
+function getTargets(config) {
+    var configLength = Object.keys(config).length;
+    for (let i = 0; i < configLength; i++) {
+        var keys = Object.keys(config);
+        if (keys[i] === "targets") {
+            if (Object.values(Object.values(config))[i].includes(".")) {
+                var targets = Object.values(Object.values(config))[i].split(
+                    " "
+                );
+            }
+        }
+    }
+    for (let i = 0; i < targets.length; i++) {
+        targets[i] = targets[i].replace(".", "");
+    }
+
+    return targets;
+}
+
+/*
+ * Get all animations and their info and return them in a usable format
+ *
+ * @param {OBJECT} config
+ * @return {ARRAY} animations
+ */
+
+function getAnimations(config) {
+    var animations = [];
+    var keys = Object.keys(config);
+    for (let i = 0; i < keys.length; i++) {
+        if (keys[i] === "animations") {
+            let animationsObject = Object.values(config)[i];
+            let delay, duration, easing, value;
+            let names = Object.keys(animationsObject);
+            for (let x = 0; x < names.length; x++) {
+                let animationName = Object.keys(animationsObject)[x];
+                let animationObject = Object.values(animationsObject)[x];
+                let amntKeys = Object.keys(animationObject).length;
+                for (let y = 0; y < amntKeys; y++) {
+                    if (Object.keys(animationObject)[y] === "delay") {
+                        delay = Object.values(animationObject)[y];
+                    } else if (Object.keys(animationObject)[y] === "duration") {
+                        duration = Object.values(animationObject)[y];
+                    } else if (Object.keys(animationObject)[y] === "easing") {
+                        easing = Object.values(animationObject)[y];
+                    } else if (Object.keys(animationObject)[y] === "value") {
+                        value = Object.values(animationObject)[y];
+                    }
+                }
+                var animation = [animationName, delay, duration, easing, value];
+                animations.push(animation);
+            }
+        }
+    }
+    return animations;
+}
+
+/*
+ * See if animation is infinite
+ *
+ * @param {OBJECT} config
+ * @return {BOOLEAN} isInfinite
+ */
+
+function isInfinite(config) {
+    var infinite;
+    var keys = Object.keys(config);
+    for (let i = 0; i < keys.length; i++) {
+        if (keys[i] === "infinite") {
+            infinite = Object.values(Object.values(config))[i];
+        }
+    }
+
+    return infinite;
+}
+
+/*
  * Get animation config into usable format
  *
  * @params {OBJECT} Animation config
@@ -6,24 +89,13 @@
  */
 
 function parseConfigInfo(config) {
-    var targets = null;
-    var animations = [];
-    var infinite = false;
-    for (let i = 0; i < Object.keys(config).length; i++) {
-        if (Object.keys(config)[i] === "targets") {
-            targets = Object.values(config)[i].split(" ");
-        } else if (Object.keys(config)[i] === "animations") {
-            var animationsLength = Object.keys(config).length - 1;
-            for (let x = 0; x < animationsLength; x++) {
-                animations.push([
-                    Object.keys(Object.values(config)[i])[x],
-                    Object.values(Object.values(config)[1])[x]
-                ]);
-            }
-        }
-    }
+    var targets = getTargets(config);
+    var animations = getAnimations(config);
+    var infinite = isInfinite(config);
 
-    return [targets, animations];
+    var parsedConfigInfo = [targets, animations, infinite];
+
+    return parsedConfigInfo;
 }
 
 /*
@@ -33,37 +105,29 @@ function parseConfigInfo(config) {
  */
 
 function applyAnimations(config) {
+    // console.log(config);
     var amntTargets = config[0].length;
     var amntAnimations = config[1].length;
-    var transforms = "translateY translateX scaleX scaleY rotate skewX skewY".split(
-        " "
-    );
+    var infinite = config[2];
 
     for (let i = 0; i < amntTargets; i++) {
-        var transformationString = "";
-        var target = config[0][i].replace(".", "");
         for (let x = 0; x < amntAnimations; x++) {
-            var animation = config[1][x][0];
-            var value = config[1][x][1];
-            for (let y = 0; y < transforms.length; y++) {
-                if (animation == transforms[y]) {
-                    transformationString += animation + "(" + value + ")";
-                }
-                if (y == transforms.length - 1) {
-                    for (
-                        let z = 0;
-                        z < document.getElementsByClassName(target).length;
-                        z++
-                    ) {
-                        document.getElementsByClassName(target)[
-                            z
-                        ].style.transform = transformationString;
-                    }
-                }
-            }
+            let target = config[0][x];
+            let animation = config[1][x][0];
+            let delay = config[1][x][1];
+            let duration = config[1][x][2];
+            let easing = config[1][x][3];
+            let value = config[1][x][4];
+            console.log(target, animation, delay, duration, easing, value);
         }
     }
 }
+
+/*
+ * User calls this to make their animation
+ *
+ * @param {OBJECT} config
+ */
 
 function animate(config) {
     var configData = parseConfigInfo(config);
@@ -73,8 +137,18 @@ function animate(config) {
 animate({
     targets: ".testElement .nine",
     animations: {
-        translateX: "150px",
-        rotate: "-60deg"
+        translateX: {
+            value: "100px",
+            delay: "1000",
+            duration: "1000",
+            easing: "ease"
+        },
+        rotate: {
+            value: "60deg",
+            delay: "500",
+            duration: "500",
+            easing: "ease"
+        }
     },
     infinite: false
 });
